3800be7905c246dbc84dfed844046597d3760066
JayBrown120 <43543960+JayBrown120@users.noreply.github.com>
2019-03-20 10:08:05 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,29 @@
+//Spell #1
+
+$(".button1").click(function(){
+ $(".geni1").fadeToggle();
+});
+
+
+
+//Spell #2
+
+$(".button2").click(function(){
+  $(".lamp").slideToggle();
+});
+
+
+
+//Spell #3
+
+$(".button3").click(function(){
+ $(".geni2").toggle();
+});
+
+$(".color").click(function(){
+    $("body").css("background-color","#7FE1F4");
+    
+});
+
+
+
